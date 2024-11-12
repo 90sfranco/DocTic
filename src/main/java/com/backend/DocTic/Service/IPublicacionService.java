3451bad14aac05eab821b5dae380f97fa0e9aa93
María José Suárez <maria_jos.suarez@uao.edu.co>
2024-11-12 16:07:29 -0500

    Servicios de obtener y obtener todos en publicacion

--- src/main/java/com/backend/DocTic/Service/IPublicacionService.java
+++ src/main/java/com/backend/DocTic/Service/IPublicacionService.java
@@ -1,7 +1,15 @@
 package com.backend.DocTic.Service;
 
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
 import com.backend.DocTic.Model.PublicacionModel;
 
 public interface IPublicacionService {
     String insertarPublicacion(PublicacionModel publicacion);
+
+    PublicacionModel obtenerPublicacion(ObjectId id);
+
+    List<PublicacionModel> obtenerPublicaciones();
 }
--- src/main/java/com/backend/DocTic/Service/PublicacionServiceImp.java
+++ src/main/java/com/backend/DocTic/Service/PublicacionServiceImp.java
@@ -1,8 +1,13 @@
 package com.backend.DocTic.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.backend.DocTic.Exception.RecursoNoEncontradoException;
 import com.backend.DocTic.Model.PublicacionModel;
 import com.backend.DocTic.Repository.IPublicacionRepository;
 
@@ -15,5 +20,17 @@ public class PublicacionServiceImp implements IPublicacionService{
     public String insertarPublicacion(PublicacionModel publicacion){
         repository.insert(publicacion);
         return "Se insertó la publicación con éxito";
+    }
+
+    @Override
+    public PublicacionModel obtenerPublicacion(ObjectId id){
+        Optional<PublicacionModel> publicacionEncontrada = repository.findById(id);
+        return publicacionEncontrada.orElseThrow(()-> new RecursoNoEncontradoException("No se encontró la publicación con ID "+id+" por favor verificar el ID"));
+    }
+
+
+    @Override
+    public List<PublicacionModel> obtenerPublicaciones() {
+        return repository.findAll();
     }
 }
